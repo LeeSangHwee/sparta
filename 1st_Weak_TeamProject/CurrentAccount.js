44d0ba0985d9217ea87521c6ff7188f0a916f623
LeeSangHwee <tkdgnl56@naver.com>
2024-08-08 19:09:57 +0900

    last commit

---
+++ 1st_Weak_TeamProject/CurrentAccount.js
@@ -0,0 +1,18 @@
+let myID = '';
+let myPW = '';
+
+export function GetID() {    
+    return myID;
+}
+
+export function GetPW() {
+    return myPW;
+}
+
+export function SetID(newID) {
+    myID = newID;
+}
+
+export function SetPW(newPW) {
+    myPW = newPW;
+}
